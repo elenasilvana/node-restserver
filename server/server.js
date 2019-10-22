2176ae3b74c64f99e3eb327b1f357f09b86f0d93
elenasilvana <stenocereus.queretaroensis@gmail.com>
2019-10-22 11:04:30 -0500

    separating routes from server file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,33 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function (req, res){
+app.use(require('./routes/user'));
 
-    res.json ('get usuario');
-});
+ 
 
-app.post('/usuario', function (req, res){
-    let body = req.body;
-
-    if( body.nombre === undefined ){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        })
-    }
-
-    res.json ({
-        body
-    });
-});
-
-app.put('/usuario/:id', function(req, res){
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) =>{
+    if( err ) throw err;
+    console.log('base de datos online!')
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log('escuchando puerto: ', process.env.PORT);
