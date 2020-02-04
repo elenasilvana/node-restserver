2b053e614682fdd699bb555a89a1dd205ac5e295
Elena Casillas <stenocereus.queretaroensis@gmail.com>
2020-02-04 12:31:52 -0600

    env variables added

--- server/config/config.js
+++ server/config/config.js
@@ -13,8 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://mochi:R5i3nqb8fZYBQjGW@bqback-5moin.mongodb.net/test?retryWrites=true&w=majority";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
